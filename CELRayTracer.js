df941f7540310a72e5b0ad57ca345b05ab04374d
Larry Gates <gateslm@rose-hulman.edu>
2016-11-04 23:56:09 -0400

    Adding textures and other details
    Added the ground and started adding more to the textures

--- CELRayTracer.js
+++ CELRayTracer.js
@@ -5,6 +5,8 @@ var colorLoc;
 var modelviewLoc;
 var projectionLoc;
 
+var grassTexture;
+
 
 var shapeList = [];
 var vertices = [];
@@ -15,7 +17,6 @@ var angles = [];
 var c = [];
 var s = [];
 
-
 var SHADOW_COLOR = vec4(.1, .1, .1, 1.0);
 var LIGHT1_X = 50;
 var LIGHT2_X = -50;
@@ -23,9 +24,11 @@ var LIGHT1_Y = 50;
 var LIGHT2_Y = 50;
 var LIGHT1_Z = 50;
 var LIGHT2_Z = 50;
-var CAMERA = vec3(0, 100, 300);
+var CAMERA = vec3(160, 600, 160);
 var AT = vec3(0.0, 0.0, 0.0);
 var UP = vec3(0.0, 0.0, 1.0);
+var TEXSIZE = 256;
+var NUMCHECKS = 8;
 
 
 var light1 = vec3(LIGHT1_X, LIGHT1_Y, LIGHT1_Z);
@@ -33,8 +36,10 @@ var light2 = vec3(LIGHT2_X, LIGHT2_Y, LIGHT2_Z);
 
 var projection;
 var modelView;
-var shadowProjection;
+var shadowProjection1;
+var shadowProjection2;
 var aspect;
+
 
 var Shape = function () {
 	// the gl.[Draw Type]
@@ -45,7 +50,36 @@ var Shape = function () {
 	this.length = -1;
 	// Color of object
 	this.color = vec4(1.0, 1.0, 1.0, 1.0);
+	// Ignore shadow 
+	this.ignoreShadow = false;
 };
+
+function configureGroundTexture() {
+	
+	var image1 = new Uint8Array(4*TEXSIZE * TEXSIZE);
+	for ( var i = 0; i < TEXSIZE; i++ ) {
+		for ( var j = 0; j <TEXSIZE; j++ ) {
+			var patchx = Math.floor(i/(TEXSIZE/NUMCHECKS));
+			var patchy = Math.floor(j/(TEXSIZE/NUMCHECKS));
+			if(patchx%2 ^ patchy%2) c = 255;
+			else c = 0;
+			var loc = 4*i*TEXSIZE+4*j;
+			image1[loc] = c;
+			image1[loc+1] = c;
+			image1[loc+2] = c;
+			image1[loc+3] = 255;
+		}
+	}
+	grassTexture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, grassTexture);
+	gl.pixelStorei(gl.UNPACK_COLORSPACE_CONVERSION_WEBGL, gl.NONE);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, TEXSIZE, TEXSIZE, 0, gl.RGBA, 
+		gl.UNSIGNED_BYTE, image1);
+	gl.generateMipmap(gl.TEXTURE_2D);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, 
+		gl.NEAREST_MIPMAP_LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}
 
 window.onload = function init() 
 {
@@ -71,49 +105,53 @@ window.onload = function init()
 	theta[1] = 0;
 	theta[2] = 0;
 	
-/* 	var shape = new Shape();
-	shape.drawType = gl.TRIANGLES;
-	shape.startIndex = 0;
-	shape.length = 3;
-	shape.color = vec4(0,1,0,1);
-	
-	vertices.push(vec4(0,50,0,1));
-	vertices.push(vec4(0,0,0,1));
-	vertices.push(vec4(50,50,0,1));
-	
-	shapeList.push(shape); */
-	
-	
 	// Z-axis arrow
-	var line = new Shape();
-	line.drawType = gl.LINES;
-	line.startIndex = vertices.length;
-	line.length = 2;
-	line.color = vec4(1, 0, 0, 1);
+	var lineZ = new Shape();
+	lineZ.drawType = gl.LINES;
+	lineZ.startIndex = vertices.length;
+	lineZ.length = 2;
+	lineZ.color = vec4(1, 0, 0, 1);
 	vertices.push(vec4(0, 0, 0, 1));
 	vertices.push(vec4(0, 0, 100, 1));
-	shapeList.push(line);
+	shapeList.push(lineZ);
 	
 	// Y-axis arrow
-	var line = new Shape();
-	line.drawType = gl.LINES;
-	line.startIndex = vertices.length;
-	line.length = 2;
-	line.color = vec4(0, 0, 1, 1);
+	var lineY = new Shape();
+	lineY.drawType = gl.LINES;
+	lineY.startIndex = vertices.length;
+	lineY.length = 2;
+	lineY.color = vec4(0, 0, 1, 1);
 	vertices.push(vec4(0, 0, 0, 1));
 	vertices.push(vec4(0, 100, 0, 1));
-	shapeList.push(line);
+	shapeList.push(lineY);
 	
 	// X-axis arrow
-	var line = new Shape();
-	line.drawType = gl.LINES;
-	line.startIndex = vertices.length;
-	line.length = 2;
-	line.color = vec4(0, 1, 0, 1);
+	var lineX = new Shape();
+	lineX.drawType = gl.LINES;
+	lineX.startIndex = vertices.length;
+	lineX.length = 2;
+	lineX.color = vec4(0, 0, 0, 1);
 	vertices.push(vec4(0, 0, 0, 1));
 	vertices.push(vec4(100, 0, 0, 1));
+	lineX.ignoreShadow = true;
+	shapeList.push(lineX);
 	
-	shapeList.push(line);
+	
+	// Create ground
+	
+	var ground = new Shape();
+	ground.drawType = gl.TRIANGLES;
+	ground.startIndex = vertices.length;
+	ground.length = 6;
+	ground.color = vec4(0, 1, .1, 1);
+	vertices.push(vec4(1000, 1000, 0, 1));
+	vertices.push(vec4(1000, -1000, 0, 1));
+	vertices.push(vec4(-1000, 1000, 0, 1));
+	vertices.push(vec4(1000, -1000, 0, 1));
+	vertices.push(vec4(-1000, 1000, 0, 1));
+	vertices.push(vec4(-1000, -1000, 0, 1));
+	ground.ignoreShadow = true;
+	shapeList.push(ground);
 	
 	// Load shaders and initialize attribute buffers
 	
@@ -132,6 +170,19 @@ window.onload = function init()
 	gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
 	gl.enableVertexAttribArray( vPosition );
 	
+	/* var tBuffer = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer);
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
+
+	var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+	gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vTexCoord );
+	
+	configureGroundTexture();
+	
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, grassTexture);
+	gl.uniform1i(gl.getUniformLocation(program, "Tex0"), 0); */
 	
 	render();
 }
@@ -177,7 +228,7 @@ function render() {
 					CAMERA, // Eye
 					AT, // At
 					UP); // Up
-	projection = perspective(45.0, aspect, 1, 20 * 100);
+	projection = perspective(35.0, aspect, 1, 20 * 100);
 	var rotation = mult(rz, mult(ry, rx));
 	modelView = mult(looking, mult(tz2, mult(rotation, tz1)));
 	
@@ -187,13 +238,40 @@ function render() {
 	
 	for ( i = 0; i < shapeList.length; i++) {
 		gl.uniform4fv(colorLoc, shapeList[i].color);
-		gl.drawArrays(shapeList[i].drawType, shapeList[i].startIndex, shapeList[i].length);
+		gl.drawArrays(shapeList[i].drawType, shapeList[i].startIndex, 
+			shapeList[i].length);
 	}
 	
 	
 	// Set up shadow projection
-	shadowProjection = mat4();
-	shadowProjection[3][3] = 0;
-	shadowProjection[3][1] = -1/light1[1];
+	shadowProjection1 = mat4();
+	shadowProjection1[3][3] = 0;
+	shadowProjection1[3][1] = -1/light1[1];
+	modelView = mult(modelView, translate(light1[0], light1[1], light1[2]));
+	modelView = mult(modelView, shadowProjection1);
+	modelView = mult(modelView, translate(-light1[0], -light1[1], -light1[2]));
+	gl.uniformMatrix4fv(modelViewLoc, false, flatten(modelView));
+	gl.uniform4fv(colorLoc, SHADOW_COLOR);
+	for(var i = 0; i < shapeList.length; i++) {
+		if (shapeList[i].length != 6) { // Handle the ground issue
+			gl.drawArrays(shapeList[i].drawType, shapeList[i].startIndex, 
+				shapeList[i].length);
+		}
+	}
 	
+	shadowProjection2 = mat4();
+	shadowProjection2[3][3] = 0;
+	shadowProjection2[3][1] = -1/light2[1];
+	modelView = mult(modelView, translate(light2[0], light2[1], light2[2]));
+	modelView = mult(modelView, shadowProjection2);
+	modelView = mult(modelView, translate(-light2[0], -light2[1], -light2[2]));
+	gl.uniformMatrix4fv(modelViewLoc, false, flatten(modelView));
+	gl.uniform4fv(colorLoc, SHADOW_COLOR);
+	for(var i = 0; i < shapeList.length; i++) {
+		if (!shapeList[i].ignoreShadow) { // Handle the ground issue
+			gl.drawArrays(shapeList[i].drawType, shapeList[i].startIndex, 
+				shapeList[i].length);
+		}
+	}
+	render;
 }
